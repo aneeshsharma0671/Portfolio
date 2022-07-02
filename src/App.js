91903cde5cd7ec73c0ffc1a653f50b2582d03aa7
Aneesh Sharma <aneeshsharma0671@gmail.com>
2022-07-03 00:56:09 +0530

    Repo test

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ function App() {
         <p>
           Hi, I am a React App.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
