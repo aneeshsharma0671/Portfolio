35be185b6023b21ca6e6a869e432679c94595b55
Aneesh Sharma <aneeshsharma0671@gmail.com>
2022-07-18 00:10:09 +0530

    Updated projects page

--- src/Pages/projects.js
+++ src/Pages/projects.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
-
+import './projects.css'
+import Card from '../Components/Cards/projectCard'
 export class project extends Component {
 
   render() {
+    const projectLogos = ['/ProjectLogos/Pandemic.png','/ProjectLogos/Flappy.png'];
+
     return (
-      <div>project</div>
+      <div className='projects'>
+        <div className='projects__header'>Projects</div>
+        <div className='cards'>
+          <Card img={process.env.PUBLIC_URL + projectLogos[0]} title='Pandemic Simulator' author='Unity' />
+          <Card img={process.env.PUBLIC_URL + projectLogos[1]} title='Flappy Bird AI' author='Unity' />
+        </div>
+      </div>
     )
   }
 }
